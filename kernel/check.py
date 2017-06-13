060734010dd19e44e52cefa592d6f30bb79e4d3d
Alice Ferrazzi <alicef@gentoo.org>
2017-06-13 11:51:02 +0900

    added uname command for check kernel version

--- kernel/check.py
+++ kernel/check.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
+import os
 
 
 class KernelCheck():
     def __init__(self):
+        pass
+
+    def version(self):
+
+        for i in os.uname():
+            print(i)
         pass
