815469dbdde010dbadd6d19fafa4114f23a1f2c7
Alice Ferrazzi <alicef@gentoo.org>
2017-06-14 09:49:52 +0900

    Added github clone function

--- client/checkers.py
+++ client/checkers.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import os
+from git import Repo
 
 
 class Kernel():
@@ -11,10 +12,24 @@ class Kernel():
         pass
 
     def get_version(self):
-        import pprint
         tmp = os.uname()[2].split(".")
         self.major = tmp[0]
         self.minor = tmp[1]
         tmp[2] = tmp[2].split("-")
         self.revision = tmp[2][0]
         pass
+
+
+class CVE():
+
+    def __init__(self):
+        self.git_url = ""
+        self.repo_dir = ""
+        pass
+
+    def download(self):
+        Repo.clone_from(self.git_url, self.repo_dir)
+
+    def set_repo(self, git_url, repo_dir):
+        self.git_url = git_url
+        self.repo_dir = repo_dir
