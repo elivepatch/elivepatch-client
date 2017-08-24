d0779ca96303c4d83a92c0fcee24a1f8a3a38550
Alice Ferrazzi <alice.ferrazzi@gmail.com>
2017-08-25 02:31:11 +0900

    fixing setup.py file

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='elivepatch_client',
+    name='elivepatch-client',
     version='0.01',
-    description='Patching differents gentoo distributions',
-    url='https://gentoo.org',
+    description='Distributed live patch client and automatic kernel live patch for kernel CVE',
+    url='https://wiki.gentoo.org/wiki/User:Aliceinwire/elivepatch, ' +\
+        'https://github.com/aliceinwire/elivepatch-client',
     author='Alice Ferrazzi',
     author_email='alice.ferrazzi@gmail.com',
     license='GNU GPLv2+',
     packages=find_packages(),
+    scripts=['bin/elivepatch'],
 )
